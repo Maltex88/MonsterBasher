665c3ff0491b5e3cb4e2bbc698797770b59dc076
unknown <henrik.nilsson88@gmail.com>
2019-01-04 10:13:42 +0100

    More fightMechanics

--- fight.js
+++ fight.js
@@ -1,6 +1,10 @@
 let player = {
   hp: 100,
-  dmg: 10
+  dmg: 10,
+  Abillity1: "Arrow",
+  Abillity2: "Fire Arrow",
+  Abillity3: "Lightning Arrow",
+  Abillity4: "Cold Arrow"
 }
 
 let enemy = {
@@ -8,66 +12,61 @@ let enemy = {
   dmg: 10
 }
 
-//arrow-button fire-arrow-button rain-of-arrow-button cold-arrow-button
-
+/*used later for disable button*/
 let attack1 =  document.getElementById('attack1');
 let attack2 = document.getElementById('attack2');
 let attack3 = document.getElementById('attack3');
 let attack4 = document.getElementById('attack4');
+/*used later for disable button*/
 
+/*used later for classes so that the ability in player object is displayed
+the attack button, to switch spells just change ability1-4*/
+attack1.innerText = ""+ player.Abillity1 +"";
+attack2.innerText = ""+ player.Abillity2 +"";
+attack3.innerText = ""+ player.Abillity3 +"";
+attack4.innerText = ""+ player.Abillity4 +"";
+/*used later for classes*/
+
+/*Attack functions*/
   const attackFunction1 = () => {
-    let messageToPlayer = document.getElementById('msgToPlayer');
+    let messageToPlayerAttack = document.getElementById('msgToPlayerAttack');
+    let messageToPlayerAttacked = document.getElementById('msgToPlayerAttacked');
+    //Give the player attack information, maybe later make a combatlog?
 
+    //Players Combat starts
     let playerAttack = Math.floor(Math.random()  * player.dmg + 5);
-    console.log(playerAttack);
+    //hits the target, change the + 5 for more or less dmg abilitys
     enemy.hp -= playerAttack;
+    messageToPlayerAttack.innerText = "You strike your opponent with your " + player.Abillity1 + " ability and did "+ playerAttack +" damage!"
     printToScreen();
       attack1.disabled = true;
       attack2.disabled = true;
       attack3.disabled = true;
       attack4.disabled = true;
-      messageToPlayer.innerText = "Opponent is about to strike";
-      //messageToPlayer.innerText = "Opponent strike you for "+ motståndare.skada +" damage";
-      //setTimeout(() => )
+    //Players Combat ends
+
+    //enemy combat start
+      setTimeout(() => {
+        let enemyAttack = Math.floor(Math.random()  * enemy.dmg);
+        player.hp -= enemyAttack;
+        msgToPlayerAttacked.innerText = "Your opponent strikes you for "+ enemyAttack +" damage!";
+
+          printToScreen();
+          attack1.disabled = false;
+          attack2.disabled = false;
+          attack3.disabled = false;
+          attack4.disabled = false;
+      }, 1500 /*valu to change time it takes for combat*/);
 
   }
   const attackFunction2 = () => {
-    let messageToPlayer = document.getElementById('msgToPlayer');
+    let messageToPlayerAttack = document.getElementById('msgToPlayerAttack');
+    let messageToPlayerAttacked = document.getElementById('msgToPlayerAttacked');
 
     let playerAttack = Math.floor(Math.random()  * player.dmg + 5);
     console.log(playerAttack);
     enemy.hp -= playerAttack;
-    printToScreen();
-      attack1.disabled = true;
-      /*attack2.disabled = true;
-      attack3.disabled = true;
-      attack4.disabled = true;*/
-      messageToPlayer.innerText = "Opponent is about to strike";
-      //messageToPlayer.innerText = "Opponent strike you for "+ motståndare.skada +" damage";
-      //setTimeout(() => )
-  }
-  const attackFunction3 = () => {
-    let messageToPlayer = document.getElementById('msgToPlayer');
-
-    let playerAttack = Math.floor(Math.random()  * player.dmg + 5);
-    console.log(playerAttack);
-    enemy.hp -= playerAttack;
-    printToScreen();
-      attack1.disabled = true;
-      /*attack2.disabled = true;
-      attack3.disabled = true;
-      attack4.disabled = true;*/
-      messageToPlayer.innerText = "Opponent is about to strike";
-      //messageToPlayer.innerText = "Opponent strike you for "+ motståndare.skada +" damage";
-      //setTimeout(() => )
-
-  }
-  const attackFunction4 = () => {
-    let messageToPlayer = document.getElementById('msgToPlayer');
-
-    let playerAttack = Math.floor(Math.random()  * player.dmg + 5);
-    console.log(playerAttack);
-    enemy.hp -= playerAttack;
+    messageToPlayerAttack.innerText = "You strike your opponent with your " + player.Abillity2 + " ability and did "+ playerAttack +" damage!"
     printToScreen();
       attack1.disabled = true;
       attack2.disabled = true;
@@ -79,8 +78,36 @@ let attack4 = document.getElementById('attack4');
       setTimeout(() => {
         let enemyAttack = Math.floor(Math.random()  * enemy.dmg);
         player.hp -= enemyAttack;
-        messageToPlayer.innerText = "Your opponent strikes you for "+ enemyAttack +" damage!";
-        console.log(enemyAttack);
+        msgToPlayerAttacked.innerText = "Your opponent strikes you for "+ enemyAttack +" damage!";
+
+          printToScreen();
+          attack1.disabled = false;
+          attack2.disabled = false;
+          attack3.disabled = false;
+          attack4.disabled = false;
+      }, 1500 );
+  }
+  const attackFunction3 = () => {
+    let messageToPlayerAttack = document.getElementById('msgToPlayerAttack');
+    let messageToPlayerAttacked = document.getElementById('msgToPlayerAttacked');
+
+    let playerAttack = Math.floor(Math.random()  * player.dmg + 5);
+    console.log(playerAttack);
+    enemy.hp -= playerAttack;
+    messageToPlayerAttack.innerText = "You strike your opponent with your " + player.Abillity3 + " ability and did "+ playerAttack +" damage!"
+    printToScreen();
+      attack1.disabled = true;
+      attack2.disabled = true;
+      attack3.disabled = true;
+      attack4.disabled = true;
+
+      //messageToPlayer.innerText = "Opponent strike you for "+ motståndare.skada +" damage";
+
+      setTimeout(() => {
+        let enemyAttack = Math.floor(Math.random()  * enemy.dmg);
+        player.hp -= enemyAttack;
+        msgToPlayerAttacked.innerText = "Your opponent strikes you for "+ enemyAttack +" damage!";
+
           printToScreen();
           attack1.disabled = false;
           attack2.disabled = false;
@@ -89,8 +116,38 @@ let attack4 = document.getElementById('attack4');
       }, 1500 );
 
   }
+  const attackFunction4 = () => {
+    let messageToPlayerAttack = document.getElementById('msgToPlayerAttack');
+    let messageToPlayerAttacked = document.getElementById('msgToPlayerAttacked');
 
+    let playerAttack = Math.floor(Math.random()  * player.dmg + 5);
+    console.log(playerAttack);
+    enemy.hp -= playerAttack;
+    messageToPlayerAttack.innerText = "You strike your opponent with your " + player.Abillity4 + " ability and did "+ playerAttack +" damage!"
+    printToScreen();
+      attack1.disabled = true;
+      attack2.disabled = true;
+      attack3.disabled = true;
+      attack4.disabled = true;
 
+      //messageToPlayer.innerText = "Opponent strike you for "+ motståndare.skada +" damage";
+
+      setTimeout(() => {
+        let enemyAttack = Math.floor(Math.random()  * enemy.dmg);
+        player.hp -= enemyAttack;
+        msgToPlayerAttacked.innerText = "Your opponent strikes you for "+ enemyAttack +" damage!";
+
+          printToScreen();
+          attack1.disabled = false;
+          attack2.disabled = false;
+          attack3.disabled = false;
+          attack4.disabled = false;
+      }, 1500 );
+
+  }
+/*Attack functions*/
+
+/*Write out new value of enemy and player*/
 const printToScreen = () => {
   document.getElementById('enemy-hp').innerText =
   enemy.hp;
@@ -100,3 +157,4 @@ const printToScreen = () => {
 }
 
 printToScreen();
+/*Write out new value of enemy and player*/
