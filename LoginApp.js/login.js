df2a41c03f5c59b826ec6716dd69cb4f4f496aa1
IbraCadaBra010101 <44027882+IbraCadaBra010101@users.noreply.github.com>
2019-01-08 14:31:58 +0100

    Lade till lite kommentarer bara

--- LoginApp.js/login.js
+++ LoginApp.js/login.js
@@ -3,20 +3,17 @@
     let uiConfig = {
         callbacks: {
             signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-                // User successfully signed in.
-                // Return type determines whether we continue the redirect automatically
-                // or whether we leave that to developer to handle.
+               
                 return true;
             },
             uiShown: function() {
-                // The widget is rendered.
-                // Hide the loader.
+               
                 document.getElementById('loader').style.display = 'none';
             }
         },
-        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+       
         signInFlow: 'popup',
-        signInSuccessUrl: 'main.html',
+        signInSuccessUrl: 'main.html', // ändra till relevant html sida.
         signInOptions: [
             //Leave the lines as is for the providers you want to offer your users.
             //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -26,9 +23,9 @@
             firebase.auth.EmailAuthProvider.PROVIDER_ID,
             //firebase.auth.PhoneAuthProvider.PROVIDER_ID
         ],
-        // Terms of service url.
+      
         tosUrl: '<your-tos-url>',
-        // Privacy policy url.
+        
         privacyPolicyUrl: 'main.html'
     };
 
