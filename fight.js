8b961e4611e85709b89fb0babf24c508d72c807b
Kamphol Taeng-iam <kamphol_23@hotmail.se>
2019-01-15 12:34:30 +0100

    nothing

--- fight.js
+++ fight.js
@@ -192,17 +192,16 @@ $(document).ready(() => {
   function warriorMoves(){
       document.getElementById("playerImg").src = "picture/knight_attack.gif";
       document.getElementById('player').id = 'playerMove';
-      if(player.class = "warrior"){
           setTimeout(() => {
               document.getElementById("playerImg").src = "picture/knight_idle.gif";
               document.getElementById('playerMove').id = 'player';
           }, 1000)
-      }
   }
+
   $('#attack1').click(function(){
     msgToPlayer();
     enemyAttacked1();
-     isThisAWarrior();
+    isThisAWarrior();
 
       if (isGameOver(enemy.hp)){
         enemyKilled();
